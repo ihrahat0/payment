34c3d368fcd99fa13600a85187a29c9deb584f13
IH RAHAT <ihrahat@IHs-MacBook-Air.local>
2025-04-24 14:30:55 +0600

    Delete providers tsx

--- src/app/providers.tsx
+++
@@ -1,3 +0,0 @@
-// This file is no longer used. 
-// Web3 functionality has been migrated to src/context/index.tsx using Reown AppKit.
-// The file is kept to prevent import errors until all references are updated. 
